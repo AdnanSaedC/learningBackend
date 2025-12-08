c8b539ae6ad27d28de3d1ea22db0f3244b670681
AdnanSaedC <adnansaed180@gmail.com>
2025-12-08 21:53:52 +0530

    structure changed

--- backend/02_express/index.js
+++ backend/02_express/index.js
@@ -1,39 +1,8 @@
 import express from 'express';
 
-import logger from "./02_logger.js";
-import morgan from "morgan";
-
-//we are extracting these data from url
-const morganFormat = ":method :url :status :response-time ms";
-
-
-
 const app = express();
 const port = 3000;
 app.use(express.json())
-
-//here we are telling the express to pass through this every request
-app.use(
-
-//craetes a morgon logger which will send by winston(in my case here) later
-  morgan(morganFormat, {
-    //take the stream of data and write
-    stream: {
-
-    //here we are just splitting the msg and assigning the value to each component
-      write: (message) => {
-        const logObject = {
-          method: message.split(" ")[0],
-          url: message.split(" ")[1],
-          status: message.split(" ")[2],
-          responseTime: message.split(" ")[3],
-        };
-        logger.info(JSON.stringify(logObject));
-        //we have a winston obect which contains info method which we are calling here
-      },
-    },
-  })
-);
 //it is used to convert raw body data into json object for future use
 
 const data=[];
