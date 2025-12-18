30712082c5f72cd18746fdcfae9ba1aba5e6d499
AdnanSaedC <adnansaed180@gmail.com>
2025-12-19 00:08:28 +0530

    tweet controller got updated

--- 03_vidtube/src/controllers/video.controller.js
+++ 03_vidtube/src/controllers/video.controller.js
@@ -5,6 +5,7 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { use } from "react"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
@@ -18,23 +19,83 @@ const publishAVideo = asyncHandler(async (req, res) => {
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
     //TODO: get video by id
+    const { videoId } = req.params
+
+    if(!videoId){
+        throw new ApiError(400,"VideoId was missing")
+    }
+
+    const video = await Video.findById(videoId);
+
+    if(!video){
+        throw new ApiError(400,"Video not found")
+    }
+
+    return res
+            .status(200)
+            .json(new ApiResponse(200,video,"Video fetched successfully"))
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    const { videoId } = req.params
+    const user = req?.user?._id
+    const {description,thumbnail} = req.body
+
+    if(!videoId || !user){
+        throw new ApiError(400,"videoId or user is missing")
+    }
+    
 
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
     //TODO: delete video
+    const { videoId } = req.params
+    const user = req?.user?._id
+
+    if(!videoId || !user){
+        throw new ApiError(400,"videoId or user is missing")
+    }
+
+    const video = await Video.findOneAndDelete({
+        owner:user,
+        _id:videoId
+    })
+
+    if(!video){
+        throw new ApiError(400,"Video not found or unauthorized")
+    }
+
+    return res
+            .status(200)
+            .json(new ApiResponse(200,"video was deleted successfully"))
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+    const user = req?.user?._id
+
+    if(!user || !videoId){
+        throw new ApiError(400,"userId or videoId is missing")
+    }
+
+    const video = await Video.findOne({
+        _id:videoId,
+        owner:user
+    })
+
+    if(!video){
+        throw new ApiError(400,"failed to get video or unauthorized")
+    }
+
+    video.isPublished = !video.isPublished
+    await video.save({validateBeforeSave:false})
+
+    return res
+            .status(200)
+            .json(new ApiResponse(200,"publish status got toggled"))
 })
 
 export {
