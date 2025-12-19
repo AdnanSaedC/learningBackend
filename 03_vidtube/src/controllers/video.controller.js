03fcf194165144c1d5d95e44bbf719d232d7ab72
AdnanSaedC <adnansaed180@gmail.com>
2025-12-20 00:26:46 +0530

    all controllers are over now routes

--- 03_vidtube/src/controllers/video.controller.js
+++ 03_vidtube/src/controllers/video.controller.js
@@ -5,17 +5,105 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
-import { use } from "react"
+
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
+    // const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    // //TODO: get all videos based on query, sort, pagination
+
+    let { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    
+    page = parseInt(page)
+    limit = parseInt(limit)
+    
+
+    const pipeline = []
+    //there is a exist this order in mongodb first search then match then other operation
+    if(query){
+        pipeline.push(
+            {
+                $search:{
+                    index:'default',
+                    text:{
+                        query:query,
+                        path:'description'
+                    }
+                }
+            }
+        )
+    }
+
+    if(userId){
+        pipeline.push({
+            $match:{
+                "owner":new mongoose.Types.ObjectId(userId)
+            }
+        })
+    }
+
+    if(sortBy){
+        pipeline.push({
+            $sort:{
+                [sortBy]: sortType === "asc" ? 1 : -1
+            }
+        })
+    }
+
+    pipeline.push(
+        {
+            $skip:(page-1)*limit
+        },
+        {
+            $limit : limit
+        }
+    )
+
+
+    const videos = await Video.aggregate(pipeline)
+
+    if(!videos){
+        throw new ApiError(400,"fail to get videos")
+    }
+
+    return res
+            .status(200)
+            .json(new ApiResponse(200,videos,"fetched videos successfuly"))
+    
+    
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    const {  description} = req.body
+    const videoLocalPath = req.files?.path
+    const user = req?.user?._id
+
+    if(!user){
+        throw new ApiError(400,"fail to get user details")
+    }
+
+    
+    const videoLink = await uploadOnCloudinary(videoLocalPath)
+    if(!videoLink?.url){
+        throw new ApiError(400,"fail to get video link")
+    }
+
+    const video = await Video.create({
+        videoFile:videoLink.url,
+        description:description,
+        owner:user,
+        thumbnail:videoLink.thumbnail || " ",
+        duration:videoLink.duration || 0
+    })
+
+    if(!video){
+        throw new ApiError(400,"Faile to upload on cloudinary")
+    }
+
+    return res
+            .status(200)
+            .json(new ApiResponse(200,video,"video uploaded successfully"))
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
---
+++ 03_vidtube/src/routes/comment.route.js
@@ -0,0 +1,4 @@
+import { Router } from "express";
+import { router } from "./healthcheck.route";
+
+const commentRouter = Router()
